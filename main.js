3ea620ad6c9b5821500aaa0f85eb025166c81dc4
pranav-git1 <77086184+pranav-git1@users.noreply.github.com>
2021-12-14 21:06:49 +0530

    uploaded

---
+++ main.js
@@ -0,0 +1,7 @@
+function image() {
+    document.getElementById("image-file").innerHTML = document.getElementById("input-file");
+}
+
+function menu_click() {
+    window.location = "personal.html";
+}
